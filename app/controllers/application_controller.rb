7db002c215e5c2cca9a06d2e427a6cf8b01a645d
ryeonthesulfur <perplexing2004@gmail.com>
2026-04-15 21:54:03 +0900

    rubocop による修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,8 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,
-                                      keys: [:nickname, :email, :password, :password_confirmation, :last_name, :first_name, :last_name_kana, :first_name_kana,
+                                      keys: [:nickname, :email, :password, :password_confirmation,
+                                             :last_name, :first_name, :last_name_kana, :first_name_kana,
                                              :birthday])
   end
 end
