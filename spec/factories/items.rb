46d5a28e296e57ad699c5f58f9a8c05f6b38bb39
ryeonthesulfur <perplexing2004@gmail.com>
2026-04-17 23:15:18 +0900

    商品情報の各クラス一部修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     category_id { Faker::Number.between(from: 2, to: 11) }
     condition_id { Faker::Number.between(from: 2, to: 7) }
     burden_id { Faker::Number.between(from: 2, to: 3) }
-    prefecture_id { Faker::Number.between(from: 2, to: 47) }
+    prefecture_id { Faker::Number.between(from: 2, to: 48) }
     shipping_days_id { Faker::Number.between(from: 2, to: 4) }
     price { Faker::Number.between(from: 300, to: 9_999_999) }
     association :user
