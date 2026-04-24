c8db733e157bc5248c06ad5f9f32d032f19ae840
ryeonthesulfur <perplexing2004@gmail.com>
2026-04-24 20:08:31 +0900

    Form オブジェクトの作成
    ・DB無しクラス Formaの作成
    ・attr_accessor の記述
    ・商品情報バリデーションの記述
    ・ActiveRecordのsave メソッドを手動で記述

---
+++ app/models/forma.rb
@@ -0,0 +1,23 @@
+class Forma
+  include ActiveModel::Model
+  attr_accessor :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :user_id, :item_id, :token
+
+  validates :postal_code, presence: true, format: { with: /\A\d{3}-\d{4}\z/ }
+  validates :prefecture_id, presence: true, numericality: { only_integer: true, other_than: 1 }
+
+  validates :city, presence: true
+  validates :address, presence: true
+
+  validates :phone_number, presence: true, format: { with: /\A\d{10,11}\z/ }
+
+  validates :user_id, presence: true
+  validates :item_id, presence: true
+  validates :token, presence: true
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+
+    order.create_address(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address,
+                         building: building, phone_number: phone_number)
+  end
+end
