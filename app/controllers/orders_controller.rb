07f8d9dd6d2faafe651772c9b4a3679e2ed0d0f9
ryeonthesulfur <perplexing2004@gmail.com>
2026-04-27 02:33:54 +0900

    コントローラー編集　その２
    ・Form オブジェクト（Forma）を保存
    ・PAY.JPでの決済処理（pay_item）
    ・公開鍵の環境変数化（JSへの橋渡し）
    ・render メソッドへの追記（公開鍵）

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,25 +1,37 @@
 class OrdersController < ApplicationController
   def index
+    gon.public_key = ENV['PAYJP_PUBLIC_KEY']
     @item = Item.find(params[:item_id])
     @forma = Forma.new
   end
 
   def create
     @item = Item.find(params[:item_id])
-    @forma = Forma.new(order_params)
+    @forma = Forma.new(forma_params)
     if @forma.valid?
+      pay_item
       @forma.save
       redirect_to root_path
     else
+      gon.public_key = ENV['PAYJP_PUBLIC_KEY']
       render :index, status: :unprocessable_entity
     end
   end
 
   private
 
-  def order_params
+  def forma_params
     params.require(:forma).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(
-      user_id: current_user.id, item_id: params[:item_id], token: params[:token]
+      user_id: current_user.id, item_id: params[:item_id], token: params[:payjp_token]
+    )
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: forma_params[:token],
+      currency: 'jpy'
     )
   end
 end
