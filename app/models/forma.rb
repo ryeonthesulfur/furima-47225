614f8478fefbf050d487f95c45b7a72d8c8f1dec
ryeonthesulfur <perplexing2004@gmail.com>
2026-04-28 17:14:28 +0900

    商品購入機能　各修正

--- app/models/forma.rb
+++ app/models/forma.rb
@@ -5,12 +5,12 @@ class Forma
   with_options presence: true do
     validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/ }
     validates :prefecture_id, numericality: { only_integer: true, other_than: 1 }
-    validates :city, presence: true
-    validates :address, presence: true
+    validates :city
+    validates :address
     validates :phone_number, format: { with: /\A\d{10,11}\z/ }
-    validates :user_id, presence: true
-    validates :item_id, presence: true
-    validates :token, presence: true
+    validates :user_id
+    validates :item_id
+    validates :token
   end
 
   def save
